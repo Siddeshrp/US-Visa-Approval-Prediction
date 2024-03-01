68504ada7cd5a91e380a07a2dc9536cfebd004a2
Siddeshrp <115860648+Siddeshrp@users.noreply.github.com>
2024-03-01 09:48:58 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,9 +1,2 @@
-from US_visa.logger import logging
-from US_visa.exception import USvisaException
-import sys
-
-try:
-    a = 1 / "10"
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys) from e
+from US_visa.constants import *
+print(DATABASE_NAME)
